9506bdb816ecab95c5caa5b0e590ce5650b6016d
Yuki Kodama <endflow.net@gmail.com>
2016-05-28 10:39:41 +0900

    Remove monitoring feature

--- throttle/store.js
+++ throttle/store.js
@@ -3,40 +3,17 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
 import rootSaga from './sagas';
 import DevTools from './dev-tools';
-import { actions } from 'redux-devtools-saga-visualizer';
-
-function createEnhancer() {
-  const enhancerFn = createStore => (reducer, initialState, enhancer) => {
-    const store = createStore(reducer, initialState, enhancer);
-    const dispatch = store.liftedStore.dispatch;
-    enhancerFn.monitor.effectTriggered = function (desc) {
-      // console.log('Saga visualizer: effectTriggered:', desc);
-      dispatch(actions.performEffect(desc));
-    };
-    enhancerFn.monitor.effectResolved = function (effectId, result) {
-      // console.log('Saga visualizer: effectResolved:', effectId, result);
-    };
-    enhancerFn.monitor.effectRejected = function (effectId, error) {
-      // console.log('Saga visualizer: effectRejected:', effectId, error);
-    };
-    enhancerFn.monitor.effectCancelled = function (effectId) {
-      // console.log('Saga visualizer: effectCancelled:', effectId);
-    };
-    return store;
-  };
-  enhancerFn.monitor = {};
-  return enhancerFn;
-}
+import SagaVisualizer from 'redux-devtools-saga-visualizer';
 
 export default function configureStore(initialState) {
-  const enhancer = createEnhancer();
-  const sagaMiddleware = createSagaMiddleware({ sagaMonitor: enhancer.monitor });
+  const sagaMonitor = SagaVisualizer.monitor();
+  const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
   const store = createStore(
     reducer,
     initialState,
     compose(
       applyMiddleware(sagaMiddleware),
-      enhancer,
+      sagaMonitor,
       DevTools.instrument()
     )
   );
